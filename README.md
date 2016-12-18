91455c16470bb5ccb17f6ae3ad762f8f647db396
ivpdev <ivan.v.panov@googlemail.com>
2016-12-18 23:28:59 +0100

    add missing compiled files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Browser game written(not yet, being under construction) in ClojureScript.
-Based on Matter.js 2d phisics library.
+Based on Matter.js 2d physics library.
 
 Demo at http://climbr-game.herokuapp.com/index.html
---
+++ game/resources/public/js/compiled/out/climbr/figures/figures.js
@@ -0,0 +1,54 @@
+// Compiled by ClojureScript 1.9.293 {}
+goog.provide('climbr.figures.figures');
+goog.require('cljs.core');
+goog.require('climbr.matter.matter');
+goog.require('climbr.utils.utils');
+climbr.figures.figures.get_boulder_color = (function climbr$figures$figures$get_boulder_color(standable_QMARK_,holdable_QMARK_){
+if(cljs.core.truth_((function (){var and__18730__auto__ = standable_QMARK_;
+if(cljs.core.truth_(and__18730__auto__)){
+return holdable_QMARK_;
+} else {
+return and__18730__auto__;
+}
+})())){
+return "#57846d";
+} else {
+if(cljs.core.truth_(standable_QMARK_)){
+return "#1abc68";
+} else {
+if(cljs.core.truth_(holdable_QMARK_)){
+return "#bcbcbc";
+} else {
+return "#e2e2e2";
+
+}
+}
+}
+});
+climbr.figures.figures.get_all_standables = (function climbr$figures$figures$get_all_standables(){
+return climbr.matter.matter.find_world_bodies.call(null,(function (p1__29851_SHARP_){
+return climbr.matter.matter.read_data.call(null,"standable",p1__29851_SHARP_) === true;
+}));
+});
+climbr.figures.figures.create_boulder = (function climbr$figures$figures$create_boulder(opts){
+var map__29854 = opts;
+var map__29854__$1 = ((((!((map__29854 == null)))?((((map__29854.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__29854.cljs$core$ISeq$)))?true:false):false))?cljs.core.apply.call(null,cljs.core.hash_map,map__29854):map__29854);
+var x = cljs.core.get.call(null,map__29854__$1,new cljs.core.Keyword(null,"x","x",2099068185));
+var y = cljs.core.get.call(null,map__29854__$1,new cljs.core.Keyword(null,"y","y",-1757859776));
+var width = (20);
+var height = (20);
+var standable_QMARK_ = climbr.utils.utils.not_nil_QMARK_.call(null,new cljs.core.Keyword(null,"standable","standable",492063634).cljs$core$IFn$_invoke$arity$1(opts));
+var holdable_QMARK_ = climbr.utils.utils.not_nil_QMARK_.call(null,new cljs.core.Keyword(null,"holdable","holdable",1363007938).cljs$core$IFn$_invoke$arity$1(opts));
+var color = climbr.figures.figures.get_boulder_color.call(null,standable_QMARK_,holdable_QMARK_);
+var base_params = new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"isStatic","isStatic",1002568003),true,new cljs.core.Keyword(null,"render","render",-1408033454),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"fillStyle","fillStyle",1168675854),color], null)], null);
+var params = (cljs.core.truth_(standable_QMARK_)?base_params:cljs.core.assoc.call(null,base_params,new cljs.core.Keyword(null,"collisionFilter","collisionFilter",-161885110),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"category","category",-593092832),"red"], null)));
+var boulder = climbr.matter.matter.bodies.rectangle(x,y,width,height,cljs.core.clj__GT_js.call(null,params));
+if(cljs.core.truth_(standable_QMARK_)){
+climbr.matter.matter.data_BANG_.call(null,"standable",true,boulder);
+} else {
+}
+
+return boulder;
+});
+
+//# sourceMappingURL=figures.js.map?rel=1482099879634
